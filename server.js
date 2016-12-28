f74ef00b5dcfd4a1c48de0b8decba5a901315608
Sam Sneddon <ssneddon@Cottonmouth-2.local>
2016-12-28 08:47:08 -0500

    updated skeleton

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -25,8 +26,28 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res){
-    res.render('index');
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
 });
 var port = 3030;
 
